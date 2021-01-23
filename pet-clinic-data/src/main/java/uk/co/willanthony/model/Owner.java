203b73e36d6d4e16b58ba1c88088e5302a588c40
will-anthony <will-anthony@hotmail.com>
2021-01-23 09:57:50 +0000

    Added AbstractMapService and implemented 3 concrete classes

--- pet-clinic-data/src/main/java/uk/co/willanthony/model/Owner.java
+++ pet-clinic-data/src/main/java/uk/co/willanthony/model/Owner.java
@@ -1,4 +1,4 @@
 package uk.co.willanthony.model;
 
-public class Owner {
+public class Owner extends Person {
 }
--- pet-clinic-data/src/main/java/uk/co/willanthony/services/CrudService.java
+++ pet-clinic-data/src/main/java/uk/co/willanthony/services/CrudService.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public interface CrudService<T, ID> {
 
     Set<T> findAll();
-    T findById(ID d);
+    T findById(ID id);
 
     T save(T object);
     void delete(T object);
